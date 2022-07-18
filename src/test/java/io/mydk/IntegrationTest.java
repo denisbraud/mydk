2d1652191f92cc794c933e78cb0fda52f67818ae
Denis Braud <denisbraud@gmail.com>
2022-07-18 19:17:49 +0200

    Initial version of mydk generated by generator-jhipster@7.8.1

---
+++ src/test/java/io/mydk/IntegrationTest.java
@@ -0,0 +1,17 @@
+package io.mydk;
+
+import io.mydk.MydkApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = MydkApp.class)
+public @interface IntegrationTest {
+}
